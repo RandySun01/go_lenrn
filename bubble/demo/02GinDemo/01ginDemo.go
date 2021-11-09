62d9ae51869b64a8f5003d20e507b531227ff096
RandySun01 <2214644978@qq.com>
2021-11-09 08:20:38 +0800

    初始Gin

--- bubble/demo/02GinDemo/01ginDemo.go
+++ bubble/demo/02GinDemo/01ginDemo.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func satHelloWeb(c *gin.Context)  {
+	// c.Json:返回JSON格式的数据
+	c.JSONP(http.StatusOK, gin.H{
+		"message": "helloWeb",
+	})
+
+}
 func main() {
-	$END$
+
+	// 创建一个默认的路由引擎
+	r := gin.Default()
+	// GET:请求方式: /helloWeb:请求路径
+	r.GET("/helloWeb", satHelloWeb)
+	// 启动HTTP服务,默认在0.0.0.0:8080服务
+	r.Run()
 }
