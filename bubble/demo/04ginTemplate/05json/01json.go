3d2bf0f5200f2fc96bbbd21a53b14bb78349fad9
RandySun01 <2214644978@qq.com>
2021-11-16 22:33:24 +0800

    返回josn

--- bubble/demo/04ginTemplate/05json/01json.go
+++ bubble/demo/04ginTemplate/05json/01json.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 func main() {
-	$END$
+	r := gin.Default()
+	r.GET("/jsonMap", func(c *gin.Context) {
+		// 方法1: 使用map
+		data := map[string]interface{}{
+			"name":     "randysun",
+			"age":      18,
+			"messages": "jsonMap",
+		}
+
+		c.JSON(http.StatusOK, data)
+
+	})
+
+	r.GET("/jsonStruct", func(c *gin.Context) {
+		// 方法2：通过结构体方式
+		type userInfo struct {
+			Name     string `json:"name"`
+			Age      int
+			Messages string
+			low      string
+		}
+		user := userInfo{
+			"randySun",
+			18,
+			"json struct",
+			"小写访问不到",
+		}
+		c.JSON(http.StatusOK, user)
+	})
+	r.Run(":9999")
 }
