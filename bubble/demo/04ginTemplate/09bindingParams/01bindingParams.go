0130b47333b183bcfdac671e489841ca29cdcaed
RandySun01 <2214644978@qq.com>
2021-11-17 23:25:01 +0800

    绑定参数

--- bubble/demo/04ginTemplate/09bindingParams/01bindingParams.go
+++ bubble/demo/04ginTemplate/09bindingParams/01bindingParams.go
@@ -1,5 +1,79 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type UserInfo struct {
+	Username string `form:"username" json:"username"`
+	Password string `form:"password" json:"password"`
+}
+
 func main() {
-	$END$
+
+	r := gin.Default()
+	// querystring方式
+	r.GET("/user", func(c *gin.Context) {
+		//username := c.Query("username")
+		//password := c.Query("password")
+		//user := userInfo{
+		//	Username: username,
+		//	Password: password,
+		//}
+		// 数据绑定
+		var userBind UserInfo // 声明变量
+
+		err := c.ShouldBind(&userBind)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			fmt.Printf("%#v\n", userBind)
+		}
+
+		c.JSON(http.StatusOK, userBind)
+
+	})
+
+	// form方式
+	r.POST("/form", func(c *gin.Context) {
+
+		// 数据绑定
+		var userBind UserInfo // 声明变量
+
+		err := c.ShouldBind(&userBind)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			fmt.Printf("%#v\n", userBind)
+		}
+
+		c.JSON(http.StatusOK, userBind)
+
+	})
+
+	// json方式
+	r.POST("/json", func(c *gin.Context) {
+
+		// 数据绑定
+		var userBind UserInfo // 声明变量
+
+		err := c.ShouldBind(&userBind)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+		} else {
+			fmt.Printf("%#v\n", userBind)
+		}
+
+		c.JSON(http.StatusOK, userBind)
+
+	})
+	r.Run(":9999")
 }
