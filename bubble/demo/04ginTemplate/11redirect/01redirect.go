a38a8a03236ebee2f0c1179ad0d80d4a92abf684
RandySun01 <2214644978@qq.com>
2021-11-17 23:25:29 +0800

    重定向

--- bubble/demo/04ginTemplate/11redirect/01redirect.go
+++ bubble/demo/04ginTemplate/11redirect/01redirect.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 func main() {
-	$END$
+	r := gin.Default()
+	// http重定向
+	r.GET("/index", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "http://www.baidu.com")
+
+	})
+
+	// 路由重定向
+	r.GET("/home", func(c *gin.Context) {
+		// 指定重定向路由
+		c.Request.URL.Path = "/homeRedirect"
+
+		// 继续后续处理
+		r.HandleContext(c)
+	})
+
+	r.GET("/homeRedirect", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "路由重定向",
+		})
+	})
+
+	r.Run(":9999")
 }
