a50cbe9136500ebb5ca0acbd13a97a7d1e6dcb11
RandySun01 <2214644978@qq.com>
2021-11-17 23:24:36 +0800

    path 参数

--- bubble/demo/04ginTemplate/08pathParams/01pathParams.go
+++ bubble/demo/04ginTemplate/08pathParams/01pathParams.go
@@ -1,5 +1,36 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 func main() {
-	$END$
+
+	r := gin.Default()
+
+	// 获取请求path URL 参数
+	// 注意URL的匹配不要冲突
+	r.GET("/user/:name/:age", func(c *gin.Context) {
+		// 获取路径参数
+		name := c.Param("name")
+		age := c.Param("age")
+		c.JSON(http.StatusOK, gin.H{
+			"name": name,
+			"age":  age,
+		})
+	})
+
+	r.GET("/blog/:year/:month/:day", func(c *gin.Context) {
+		// 获取路径参数
+		year := c.Param("year")
+		month := c.Param("month")
+		day := c.Param("day")
+		c.JSON(http.StatusOK, gin.H{
+			"year":  year,
+			"month": month,
+			"day":   day,
+		})
+	})
+	r.Run(":9999")
 }
