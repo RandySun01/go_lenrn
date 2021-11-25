d3d41808c6ebc3c31c97715aeb5e8a442b03287d
RandySun01 <2214644978@qq.com>
2021-11-25 23:06:40 +0800

    添加models

--- bubble/models/todo.go
+++ bubble/models/todo.go
@@ -1,5 +1,57 @@
-package main
+package models
 
-func main() {
-	$END$
+import (
+	"bubble/dao"
+)
+
+type Todo struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Status bool   `json:"status"`
+}
+
+// TODO curd 都在这里面
+
+// CreateATodo 创建todo
+func CreateATodo(todo *Todo) (err error) {
+	if err := dao.DB.Debug().Create(&todo).Error; err != nil {
+		return err
+	}
+	return
+}
+
+// GetAllTodo 查询
+func GetAllTodo() (todoList []*Todo, err error) {
+	if err := dao.DB.Find(&todoList).Error; err != nil {
+		return nil, err
+	}
+	return
+}
+
+// GetATodo // 查询单条记录
+func GetATodo(id string) (todo *Todo, err error) {
+
+	// 需要new
+	todo = new(Todo)
+	if err := dao.DB.Debug().Where("id=?", id).Find(&todo).Error; err != nil {
+		return nil, err
+	}
+	return
+
+}
+
+// UpdateATodo更新数据
+func UpdateATodo(todo *Todo) (err error) {
+	if err = dao.DB.Debug().Save(&todo).Error; err != nil {
+		return err
+	}
+	return
+}
+
+// 删除
+func DeleteATodo(id string) (err error) {
+	if err = dao.DB.Debug().Where("id = ?", id).Delete(Todo{}).Error; err != nil {
+		return err
+	}
+	return
 }
