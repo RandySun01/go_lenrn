1c7e35b2c27f8e85154407b04d1d3aa7a8db2a53
RandySun01 <2214644978@qq.com>
2021-11-25 23:06:18 +0800

    拆分路由

--- bubble/routers/routers.go
+++ bubble/routers/routers.go
@@ -1,5 +1,20 @@
-package main
+package routers
 
-func main() {
-	$END$
+import (
+	"bubble/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+	// 加载静态文件
+	r.Static("/static", "static")
+	// 加载模板
+	r.LoadHTMLGlob("./templates/*")
+	r.GET("/", controller.IndexHandler)
+
+	// 路由
+	TodoRouters(r)
+
+	return r
 }
--- bubble/routers/todoRouters.go
+++ bubble/routers/todoRouters.go
@@ -1,18 +1,27 @@
 package routers
 
-// 代办事项
+import (
+	"bubble/controller"
+	"github.com/gin-gonic/gin"
+)
 
-// 添加
-V1Group.POST("/todo", controller.CreateATodo)
+// todo路由
+func TodoRouters(r *gin.Engine) {
+	// 代办事项
+	v1Group := r.Group("v1")
 
-// 查看所有代办事项
-V1Group.GET("/todo", controller.GetTodoList)
-// 查看摸一个代办事项
-//v1Group.GET("/todo:id", func (c *gin.Context) {
-//
-//})
-// 修改
-V1Group.PUT("/todo/:id", controller.UpdateATodo)
+	// 添加
+	v1Group.POST("/todo", controller.CreateATodo)
 
-// 删除
-V1Group.DELETE("/todo/:id", controller.DeleteATodo)
+	// 查看所有代办事项
+	v1Group.GET("/todo", controller.GetTodoList)
+	// 查看摸一个代办事项
+	v1Group.GET("/todo:id", func(c *gin.Context) {
+
+	})
+	// 修改
+	v1Group.PUT("/todo/:id", controller.UpdateATodo)
+
+	// 删除
+	v1Group.DELETE("/todo/:id", controller.DeleteATodo)
+}
