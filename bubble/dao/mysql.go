e4c9146f4a56addb81e6a95bb24f5664afa09784
RandySun01 <2214644978@qq.com>
2021-11-25 23:11:00 +0800

    :racehorse:

--- bubble/dao/mysql.go
+++ bubble/dao/mysql.go
@@ -1,5 +1,23 @@
-package main
+package dao
 
-func main() {
-	$END$
+import (
+	"bubble/setting"
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// 连接数据库
+func InitMySQL(cfg *setting.MySQLConfig) (err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DB)
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	// 测试连接性
+	//DB.DB().Ping()
+	return
 }
