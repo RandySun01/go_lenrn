08f55efdfb7572a369cc01165f5eeab45af261d4
RandySun01 <2214644978@qq.com>
2021-11-25 23:09:53 +0800

    添加配置

--- bubble/setting/setting.go
+++ bubble/setting/setting.go
@@ -1,5 +1,27 @@
-package main
+package setting
 
-func main() {
-	$END$
+import (
+	"gopkg.in/ini.v1"
+)
+
+var Conf = new(AppConfig)
+
+// AppConfig 应用程序配置
+type AppConfig struct {
+	Release      bool          `ini:"release"`    // debug模式是否开启
+	Port         int           `ini:"port"`       // 端口
+	*MySQLConfig `ini:"mysql"` // 数据配置
+}
+
+// MySQLConfig 数据库配置
+type MySQLConfig struct {
+	User     string `ini:"user"`
+	Password string `ini:"password"`
+	DB       string `ini:"db"`
+	Host     string `ini:"host"`
+	Port     int    `ini:"port"`
+}
+
+func Init(file string) error {
+	return ini.MapTo(Conf, file)
 }
